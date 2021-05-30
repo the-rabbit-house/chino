ee3d923605a8ba323e9666c540b9a0e0d9667640
Kamil Jakubus <kamiljkbz@protonmail.com>
2021-05-31 00:16:24 +0200

    feat(android): add immersive mode

--- android/app/src/main/java/com/therabbithouse/chino/MainActivity.java
+++ android/app/src/main/java/com/therabbithouse/chino/MainActivity.java
@@ -1,6 +1,7 @@
 package com.therabbithouse.chino;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
@@ -12,6 +13,26 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    hideSystemUI();
+
     this.registerPlugin(Http.class);
+  }
+
+   private void hideSystemUI() {
+    final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+    View decorView = getWindow().getDecorView();
+
+    decorView.setSystemUiVisibility(flags);
+    decorView.setOnSystemUiVisibilityChangeListener((int visibility) ->  {
+      if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+        decorView.setSystemUiVisibility(flags);
+      }
+    });
   }
 }
