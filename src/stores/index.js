d92b2ebf8296597e6fa5a4bef0089f55df9b977c
Kamil Jakubus <kamiljkbz@protonmail.com>
2021-06-03 23:21:37 +0200

    refactor: move out default settings so they can be imported

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,11 +3,13 @@ import { writable as persistent } from "svelte-local-storage-store";
 
 import BACKENDS from "./backends";
 
-export const settings = persistent("settings", {
+export const DEFAULT_SETTINGS = {
   galleryCols: 3,
   galleryImageSize: "BASE",
   swipeDistance: 0.2,
-});
+};
+
+export const settings = persistent("settings", DEFAULT_SETTINGS);
 
 // Default to danbooru since its cors-free
 export const backend = writable(BACKENDS["danbooru"]);
