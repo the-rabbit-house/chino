b4cb560e7c0bcac4bb20f56b439d2032e0bce68f
Kamil Jakubus <kamiljkbz@protonmail.com>
2021-06-18 17:50:40 +0200

    chore: use tailwind 2.2 jit mode

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   plugins: {
+   'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
-    "postcss-preset-env": {
-      features: {
-        "nesting-rules": true,
-      },
-    },
   },
 };
