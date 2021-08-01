829c1c837f25e60d69eff360943bd8452d100020
Kamil Jakubus <kamiljkbz@protonmail.com>
2021-08-01 18:21:03 +0200

    docs: add initial type definitions for @Stores and @Stores/images

---
+++ src/stores/images.d.ts
@@ -0,0 +1,46 @@
+/**
+ * Indicates that some requests are fetching images
+ */
+export declare const fetching: boolean;
+
+/**
+ * Indicates that there is next page of current search query
+ */
+export declare const hasNextPage: boolean;
+
+/**
+ * Adds or removes (toggles) the tag from being searched
+ *
+ * @param {string} tag The tag that is being added or removed from searching
+ */
+export declare function toggleTag(tag: string, d: number): void;
+
+/**
+ * Fetches images based on provided tags then places it in global images store
+ * 
+ * @param {Array<string>} tags The tags which should be searched for
+ * @returns {boolean} boolean that indicates success
+ */
+export declare function requestImages(tags: Array<string>): boolean;
+
+/**
+ * Fetches next page of images
+ * 
+ * @returns {boolean} boolean that indicates success
+ */
+export declare function requestMoreImages(): boolean;
+
+export declare interface Image {
+  id: string,
+  file_name: string,
+  file_ext: string,
+  source: string,
+  artist: string,
+  thumbnail_url: string,
+  file_url: string,
+  width: number,
+  height: number,
+  tags: Array<string>,
+  video: boolean
+  data: any,
+}
---
+++ src/stores/index.d.ts
@@ -0,0 +1,36 @@
+import { Writable } from "svelte/store";
+import { Image } from "./images";
+
+/**
+ * Supported backends
+ */
+type Backend = "danbooru" | "gelbooru";
+
+/**
+ * Supported sizes of gallery images 
+ */
+type ImageSize = "BASE" | "SMALL" | "LARGE";
+
+export declare interface Settings {
+  backend: Backend,
+  limit: number, // Images per page
+  throttle: number, // Images loaded at once
+  galleryCols: number, // Number of columns in GalleryView
+  galleryImageSize: ImageSize, // Size of images in GalleryView
+  swipeDistance: number, // Swipe distance in % of screen width e.g 0.2 means 20% of screen
+  nsfw: boolean // Shows unsafe and explicit filters in SearchMenu
+}
+
+export declare const DEFAULT_SETTINGS: Settings;
+export declare const settings: Writable<Settings>;
+
+export declare const favorites: {
+  tags: Writable<Array<string>>,
+  images: Writable<Array<Image>>
+};
+
+export declare const images: Writable<Array<Image>>;
+export declare const image: Writable<Image>;
+
+export declare const tags: Writable<string>;
+
